6264b5560664bf3618236c5aca6acb4e4a1fc43d
dmitry741 <dmitrypavlov74@gmail.com>
2017-03-11 21:40:46 +0300

    authenticated is completed!
    home

--- userAccountManagment/mainapp/views.py
+++ userAccountManagment/mainapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, render_to_response
 
 def main(request):
 
-    my_dict = {'caption': 'Тестируем авторизацию'}
+    my_dict = {'caption': 'Тестируем авторизацию и регистрацию'}
 
-    return render_to_response('index.html', my_dict)
+    return render(request, 'index.html', my_dict)
