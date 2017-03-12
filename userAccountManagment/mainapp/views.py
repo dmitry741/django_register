21e06eaa7c264dd01704772f8b97646fd7df0d48
dmitry741 <dmitrypavlov74@gmail.com>
2017-03-12 21:26:45 +0300

    The first commit from pyCharm

--- userAccountManagment/mainapp/views.py
+++ userAccountManagment/mainapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, render_to_response
 
 def main(request):
 
-    my_dict = {'caption': 'Тестируем авторизацию и регистрацию'}
+    my_dict = {'caption': 'Тестируем авторизацию и регистрацию и подключение к GitHub'}
 
     return render(request, 'index.html', my_dict)
