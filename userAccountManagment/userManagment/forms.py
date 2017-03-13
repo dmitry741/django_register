e9a164e85c355b0ada5f8bcd0a9701c94f78fb80
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-03-13 17:16:16 +0300

    register is completed almost

--- userAccountManagment/userManagment/forms.py
+++ userAccountManagment/userManagment/forms.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class MyRegistrationForm(UserCreationForm):
-    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Имя пользователя'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'пароль'}))
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'пароль'}))
-    email = forms.EmailField(required=True, error_messages='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'email'}))
-    #first_name = forms.BooleanField(disabled=True)
+    username = forms.CharField(required=True, label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Имя пользователя'}))
+    password1 = forms.CharField(required=True, label='Пароль', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(required=True, label='Повторите пароль', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'пароль'}))
+    email = forms.EmailField(required=True, error_messages='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Email'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        #fields = ('__all__')
