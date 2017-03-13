81eb8d9eae8636a6be6b5cc99c569339e3aa6302
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-03-13 15:11:41 +0300

    Save the current work

--- userAccountManagment/mainapp/views.py
+++ userAccountManagment/mainapp/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, render_to_response
 
 def main(request):
-
-    my_dict = {'caption': 'Тестируем авторизацию и регистрацию и подключение к GitHub'}
-
-    return render(request, 'index.html', my_dict)
+    return render(request, 'index.html')
